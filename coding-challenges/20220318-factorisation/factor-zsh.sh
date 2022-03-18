fc569f131655a822acf91b798fe70a6396216b6f
Bgs <bgs@bgs.hu>
2022-03-18 17:32:48 +0100

    Factorisation challenge

---
+++ coding-challenges/20220318-factorisation/factor-zsh.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+i=${1}
+i=$((${i}*1.0))
+
+typeset -A factors
+ 
+# Find 'em
+/usr/games/primes 1 ${$((${i}**.5))%.*} | while read factor; do
+  while true; do 
+    div=$((${i}/${factor}))
+    [[ $div[-1] == "." ]] &&
+      {
+        i=${div}
+        (( factors[${factor}]+=1 ))
+        echo "\t ${factor}:$factors[${factor}]"
+      } || {
+        break
+      }
+    #echo "################# div=${div}"
+    [[ ${div} == "1." ]] && break
+  done
+  [[ ${i} == "1." ]] && break
+done
+
+[[ $div != 1 ]] && (( factors[${i}]+=1 ))
+
+# Print 'em
+echo ${(k)factors}| tr ' ' '\n' | sort -n | egrep -v "^1.$"| while read f; do
+  factorString=${factorString}$(printf "%s^%s " $f $factors[$f])
+done
+echo ${factorString% }
